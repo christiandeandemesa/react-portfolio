c5da209ce99f420f3c38d9a7248a31dd01dce04d
Christian Dean <christiandeandemesa@gmail.com>
2022-04-21 21:58:36 -0700

    about animation

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,14 +9,18 @@ function About() {
 			<div id={styles.about}>
 				<p id={styles.title}>&lt;h1&gt; About &lt;/h1&gt;</p>
 
-				<div id={styles.text}>
-					<h3>
-						Hello, I'm Christian Demesa, an up-and-coming React Devloper! I am a father
-						of two wonderful boys, and my hobbies include playing piano, hiking, and
-						fiddling around with React libraries. I have a B.A. in philosophy and
-						transitioned over to React full-stack development. My skills include HTML5,
-						CSS3, JavaScript 8/11, Sass, Bootstrap, jQuery, Typescript, MERN, Redux,
-						MySQL, and Git.
+				<div id={styles.text_container}>
+					<h3 id={styles.text_top}>
+						Hello, I'm Christian Demesa, an up-and-coming React Developer!
+					</h3>
+					<h3 id={styles.text_mid}>
+						I am a father of two wonderful boys, and my hobbies include playing piano,
+						hiking, and fiddling around with React libraries. I have a B.A. in
+						philosophy and transitioned over to React full-stack development.
+					</h3>
+					<h3 id={styles.text_bot}>
+						My skills include HTML5, CSS3, JavaScript 8/11, Sass, Bootstrap, jQuery,
+						Typescript, MERN, Redux, MySQL, and Git.
 					</h3>
 				</div>
 
